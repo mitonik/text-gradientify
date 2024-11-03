4073ba4f43688edbd4a74e9d1fc1af297d471408
Michał Antonik <xmitonikx@gmail.com>
2024-11-03 21:04:40 +0100

    feat: add react compiler eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -17,6 +18,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,6 +26,7 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "react-compiler/react-compiler": "error",
     },
   },
   eslintConfigPrettier,
