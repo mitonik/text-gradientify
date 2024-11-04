f2a63e99e96934742d8cd1d577a5a5d39089dd60
Michał Antonik <xmitonikx@gmail.com>
2024-11-04 23:07:43 +0100

    feat: add text transform options

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,3 +1,4 @@
+import { Button } from "primereact/button";
 import { InputTextarea } from "primereact/inputtextarea";
 
 interface InputProps {
@@ -8,11 +9,50 @@ interface InputProps {
 export function Input(props: InputProps) {
   const { onChange, text } = props;
   return (
-    <InputTextarea
-      autoResize
-      onChange={(e) => onChange(e.target.value)}
-      style={{ width: "100%" }}
-      value={text}
-    />
+    <div className="flex flex-col gap-3">
+      <InputTextarea
+        autoResize
+        onChange={(e) => onChange(e.target.value)}
+        style={{ width: "100%" }}
+        value={text}
+      />
+      <div className="flex flex-wrap gap-1">
+        <Button
+          label="lowercase"
+          onClick={() => onChange(text.toLowerCase())}
+        />
+        <Button
+          label="UPPERCASE"
+          onClick={() => onChange(text.toUpperCase())}
+        />
+        <Button
+          label="Start Case"
+          onClick={() =>
+            onChange(
+              text
+                .toLowerCase()
+                .split(" ")
+                .map((word) => word[0].toUpperCase() + word.substring(1))
+                .join(" "),
+            )
+          }
+        />
+        <Button
+          label="Sentence case"
+          onClick={() =>
+            onChange(
+              text
+                .toLowerCase()
+                .split(". ")
+                .map(
+                  (sentence) =>
+                    sentence[0].toUpperCase() + sentence.substring(1),
+                )
+                .join(". "),
+            )
+          }
+        />
+      </div>
+    </div>
   );
 }
