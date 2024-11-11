631ab25af81b12a54f99867994a099120d06deab
Michał Antonik <xmitonikx@gmail.com>
2024-11-11 11:41:44 +0100

    fix: code color gradient steps

--- src/components/output/output.tsx
+++ src/components/output/output.tsx
@@ -112,7 +112,7 @@ export function Output(props: OutputProps) {
       const colorRange = chroma
         .scale(validColors)
         .mode(mode)
-        .colors(textLine.length);
+        .colors([...textLine].length);
 
       const gradientLine = [...textLine]
         .map((letter, index) => {
